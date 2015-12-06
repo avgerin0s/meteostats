ffd01a3269b93c5af30ad45e938399226ab11b1d
Aggelos Avgerinos <evaggelos.avgerinos@gmail.com>
2015-12-07 01:02:02 +0200

    Catch IndexError

--- main.py
+++ main.py
@@ -16,11 +16,9 @@ print "Starting..."
 default_device = '/dev/tty.HC-05-DevB'
 ser = serial.Serial(default_device, 9600)
 
-time.sleep(6)
 print "Started!"
 
 prev = []
-
 
 while True:
     buf = ser.readline()
@@ -29,24 +27,27 @@ while True:
     if len(arr) > 3:
         continue
 
-    temperature = float(arr[0])
-    humidity = float(arr[1])
-    luminosity = float(arr[2])
+    try: 
+        temperature = float(arr[0])
+        humidity = float(arr[1])
+        luminosity = float(arr[2])
 
-    validator = Validator(temperature, humidity, luminosity)
+        validator = Validator(temperature, humidity, luminosity)
 
-    if (validator.is_valid()):
-        prev = [temperature, humidity, luminosity]
-    else:
-        if len(prev) > 0:
-            arr = prev
+        if (validator.is_valid()):
+            prev = [temperature, humidity, luminosity]
         else:
-            continue
+            if len(prev) > 0:
+                arr = prev
+            else:
+                continue
 
-    print ", ".join(str(x) for x in arr)
+        print ", ".join(str(x) for x in arr)
 
-    db = DBWriter(arr[0], arr[1], arr[2])
-    db.write()
+        db = DBWriter(arr[0], arr[1], arr[2])
+        db.write()
 
-    log = LogWriter(arr[0], arr[1], arr[2])
-    log.write()
+        log = LogWriter(arr[0], arr[1], arr[2])
+        log.write()
+    except IndexError:
+        continue
